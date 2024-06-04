ac7417e9b0dd82dc7f6901b75cf0d7cd5d6abe2f
hallan-n <hallanneves01@gmail.com>
2024-06-04 01:01:15 -0300

    feat: add repositories

---
+++ app/infra/repositories.py
@@ -0,0 +1,52 @@
+from infra.connection import Connection
+from sqlalchemy import select, insert, update, delete
+
+
+class Repositories:
+    def __init__(self):
+        self.conn = Connection()
+
+    async def create(self, schema, data):
+        async with self.conn as conn:
+            try:
+                stmt = insert(schema).values(**data)
+                await conn.execute(stmt)
+                return True
+            except:
+                return False
+
+    async def update(self, schema, data):
+        async with self.conn as conn:
+            try:
+                stmt = update(schema).where(schema.c.id == data.id).values(**data)
+                await conn.execute(stmt)
+                return True
+            except:
+                return False
+
+    async def read(self, schema, id):
+        async with self.conn as conn:
+            try:
+                stmt = select(schema).where(schema.c.id == id)
+                result = await conn.execute(stmt)
+                return result
+            except:
+                return None
+
+    async def read_all(self, schema):
+        async with self.conn as conn:
+            try:
+                stmt = select(schema)
+                result = await conn.execute(stmt)
+                return result.fetchall()
+            except:
+                return None
+
+    async def delete(self, schema, id):
+        async with self.conn as conn:
+            try:
+                stmt = delete(schema).where(schema.c.id == id)
+                await conn.execute(stmt)
+                return True
+            except:
+                return False
